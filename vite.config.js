a2fa9daa64c15b975b499e6272e4301a5729ab55
Katuta Mwila <katutam.mwilam@gmail.com>
2024-12-01 21:07:06 +1300

    Hopeful bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    commonjsOptions: { transformMixedEsModules: true }
+  },  
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
